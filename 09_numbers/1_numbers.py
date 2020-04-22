1a824f1826959db87c0b6c88115baec3efad5f26
Adam Balogh <baloghbh@gmail.com>
2020-04-22 09:46:14 +0200

    Add 09_numbers

---
+++ 09_numbers/1_numbers.py
@@ -0,0 +1,18 @@
+# Integer
+quantity = 4
+
+# Float
+price = 2.5
+
+# + - * / Operations with Integer and Float
+amount = quantity * price
+print(amount)
+
+# int to float
+print(float(quantity))
+# int to string
+print(type(str(quantity)))
+# float to int
+print(int(price))
+# float to string
+print(type(str(price)))
